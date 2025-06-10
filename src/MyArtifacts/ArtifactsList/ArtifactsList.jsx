0eb2559253065b5d98c693e2eec0e39794c5252d
Sauda001 <sauda.cse8.bu@gmail.com>
2025-06-10 23:33:19 +0600

    feat: implement delete confirmation for artifacts; add details view button in ArtifactsList

--- src/MyArtifacts/ArtifactsList/ArtifactsList.jsx
+++ src/MyArtifacts/ArtifactsList/ArtifactsList.jsx
@@ -2,9 +2,45 @@ import React, { use } from 'react';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 import { Inbox } from 'lucide-react'; // Optional: for an icon
 import { Link } from 'react-router';
+import Swal from 'sweetalert2';
+import { CgDetailsMore } from 'react-icons/cg';
 
 const ArtifactsList = ({ ArtifactsCreatedByPromise }) => {
+
   const artifacts = use(ArtifactsCreatedByPromise);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert your artifact!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+
+        // start delete operation
+        fetch(`https://assignment11-server-one-gules.vercel.app/artifacts/${id}`, {
+          method: 'DELETE',
+        })
+          .then(res => res.json())
+          .then(data => {
+
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your artifact has been deleted.",
+                icon: "success"
+              });
+            }
+          })
+      }
+    })
+  };
 
   return (
     <div className="max-w-5xl mx-auto py-8 px-2">
@@ -28,6 +64,7 @@ const ArtifactsList = ({ ArtifactsCreatedByPromise }) => {
                 <th>#</th>
                 <th>Artifact</th>
                 <th>Description</th>
+                <th>View Details</th>
                 <th>Update</th>
                 <th>Delete</th>
               </tr>
@@ -50,13 +87,20 @@ const ArtifactsList = ({ ArtifactsCreatedByPromise }) => {
                     </div>
                   </td>
                   <td className="max-w-xs text-sm text-gray-500">{art.shortDescription}</td>
+                  <Link to={`/artifactDetails/${art._id}`}>
+                    <td>
+                      <button className="btn btn-sm btn-primary flex items-center gap-2 mt-3.5 hover:bg-purple-300">
+                        <CgDetailsMore size={15}></CgDetailsMore>Details
+                      </button>
+                    </td>
+                  </Link>
                   <td>
                     <button className="btn btn-sm btn-outline btn-primary flex items-center gap-2">
                       <FaEdit /> Edit
                     </button>
                   </td>
                   <td>
-                    <button className="btn btn-sm btn-error flex items-center gap-2">
+                    <button onClick={() => handleDelete(art._id)} className="btn btn-sm btn-error flex items-center gap-2">
                       <FaTrashAlt /> Delete
                     </button>
                   </td>
@@ -69,5 +113,4 @@ const ArtifactsList = ({ ArtifactsCreatedByPromise }) => {
     </div>
   );
 };
-
 export default ArtifactsList;
