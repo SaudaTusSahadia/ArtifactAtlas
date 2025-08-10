2c56f313f410f8232fccc66cf299a6a0989730d8
Sauda001 <sauda.cse8.bu@gmail.com>
2025-08-10 17:44:59 +0600

    feat: add loading animation using Lottie in Loading component

--- src/Pages/Shared/Loading.jsx
+++ src/Pages/Shared/Loading.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Lottie from 'lottie-react';
+import loader from '../../assets/loader.json';
 
 const Loading = () => {
     return (
         <div className="flex items-center justify-center h-screen w-screen">
-            <span className="loading loading-spinner text-success"></span>
-            {/* <Lottie animationData={loader} size={30}></Lottie> */}
+            {/* <span className="loading loading-spinner text-success"></span> */}
+            <Lottie animationData={loader} size={30}></Lottie>
         </div>
     );
 };
